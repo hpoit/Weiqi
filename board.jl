a0a4a624fb18203b8186a265b9a45321106c7a03
Kevin Liu <kvtliu@gmail.com>
2018-01-11 16:54:20 -0200

    test and empty board

--- board.jl
+++ board.jl
@@ -25,9 +25,11 @@ createboard(magnitude) = Board(Matrix{Empty}(magnitude, magnitude))
 magnitude = 19; cb = createboard(magnitude) # test for 361 intersections
 # make distinguished marks for star points when possible
 
+"Test board"
 cb.array[5, 8] = black # struct Move in Board State?
 cb.array[13,17] = white
 cb
 
+"Empty board"
 cb.array[1:19,1:19] = empty # [row, column]
 cb
