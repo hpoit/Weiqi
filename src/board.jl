e0b60a2f27bd3f7e2cb2379eda2c689e69d882a6
Kevin Liu <kvtliu@gmail.com>
2018-01-14 13:49:31 -0200

    separate board tests into /test

--- src/board.jl
+++ src/board.jl
@@ -24,12 +24,3 @@ createboard(magnitude) = Board(Matrix{Empty}(magnitude, magnitude))
 
 magnitude = 19; cb = createboard(magnitude) # test for 361 intersections
 # make distinguished marks for star points when possible
-
-# Test board
-cb.array[5, 8] = black # struct Move in BoardState?
-cb.array[13,17] = white
-cb
-
-# Empty board
-cb.array[1:19,1:19] = empty # [row, column]
-cb
---
+++ test/board.jl
@@ -0,0 +1,8 @@
+# Test board
+cb.array[5, 8] = black # struct Move in BoardState?
+cb.array[13,17] = white
+cb
+
+# Empty board
+cb.array[1:19,1:19] = empty # [row, column]
+cb
