04010cbbd694ee8529d58ed66c20174cbd3f072a
Kevin Liu <kvtliu@gmail.com>
2018-01-22 09:02:48 -0200

    Add Pfitzner's observation

--- src/Weiqi.jl
+++ src/Weiqi.jl
@@ -1,5 +1,5 @@
 """
-Juno's block detection is indentation based -- evaluating the first line is
+Juno's block detection is indentation based - evaluating the first line is
 equivalent to typing `module Weiqi` into the repl. Eval the file.
 """
 
