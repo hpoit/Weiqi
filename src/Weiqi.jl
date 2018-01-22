97f64e43520f4cf6af45ec42e127de845d86c3e9
Kevin Liu <kvtliu@gmail.com>
2018-01-22 09:10:46 -0200

    Make Pfitzner's observation clearer

--- src/Weiqi.jl
+++ src/Weiqi.jl
@@ -1,6 +1,6 @@
 """
 Juno's block detection is indentation based - evaluating the first line is
-equivalent to typing `module Weiqi` into the repl. Eval the file.
+equivalent to typing `module Weiqi` into the terminal. Eval the file.
 """
 
 module Weiqi
