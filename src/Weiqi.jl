7f2b35b0ffdb194287893f8d29ada498e78c2c7a
Kevin Liu <kvtliu@gmail.com>
2018-01-22 09:01:18 -0200

    add Pfitzner's observation

--- src/Weiqi.jl
+++ src/Weiqi.jl
@@ -1,3 +1,8 @@
+"""
+Juno's block detection is indentation based -- evaluating the first line is
+equivalent to typing `module Weiqi` into the repl. Eval the file.
+"""
+
 module Weiqi
 
 include("board.jl")
