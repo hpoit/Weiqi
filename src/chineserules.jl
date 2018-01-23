4c4080adafd097002d27aabfd717085f3d5f78f6
Kevin Liu <kvtliu@gmail.com>
2018-01-23 18:33:51 -0200

    define BFS for grouped liberties and test/BFS.jl

--- src/chineserules.jl
+++ src/chineserules.jl
@@ -31,6 +31,7 @@ function pass(np)
     # anything else?
 end
 
+"Defines cardinal directions of `np.coords`"
 function liberties(np)
     row, col = np.coords
     if np.coords[row-1, col] == Empty
@@ -47,7 +48,40 @@ function liberties(np)
     end
 end
 
-function group end
+"Breadth-first search for grouped liberties of colors (vertices)"
+function BFS(board, np, destination)
+    row, col = np.coords
+    distance = Dict(np => 0)
+    queue = [np]
+    while !isempty(queue)
+        current = shift!(queue)
+        if current == destination
+            return distance[destination]
+        end
+        for neighbor in board[current]
+            if !haskey(distance, neighbor)
+                distance[neighbor] = distance[current] + 1
+                push!(queue, neighbor)
+            end
+        end
+    end
+    error("$np and $destination are not connected")
+end
+
+"Create adjacency list given sequence of color and tuples of colors"
+function adjacencylist(color, colors)
+    result = Dict(c => eltype(color)[] for c in color)
+    for (a, b) in colors
+        push!(result[a], b)
+        push!(result[b], a)
+    end
+    result
+end
+
+"Link BFS to adjacency list"
+BFS(color, colors, np, destination) =
+    BFS(adjacencylist(color, colors), np, destination)
+
 function removal end
 function forbidden end
 function gameover end
